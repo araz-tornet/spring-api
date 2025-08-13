574c56561e9fd612a03754947867f2b1186fe733
Araz <araz.ibrahim@tornet.co>
2025-08-13 22:26:03 +0300

    feat: implement home page with Thymeleaf integration

--- src/main/java/com/codewithmosh/store/controllers/HomeController.java
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -1,18 +1,15 @@
 package com.codewithmosh.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
-    }
-
-    @RequestMapping("/about")
-    public String about() {
-        return "about.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Araz");
+        return "index";
     }
 }
